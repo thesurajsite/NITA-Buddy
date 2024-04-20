8aa742fecb8e4ba7bd50e8bb6eac6d4af7f1783b
Suraj <thesurajsite@gmail.com>
2024-04-21 01:17:45 +0530

    checkTokenStatus Implemented

--- app/src/main/java/com/surajverma/nitahelpers/MainActivity.kt
+++ app/src/main/java/com/surajverma/nitahelpers/MainActivity.kt
@@ -11,13 +11,30 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.surajverma.nitahelpers.databinding.ActivityMainBinding
 import com.surajverma.nitahelpers.databinding.ActivityUserLoginBinding
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private lateinit var SharedPreferencesManager: SharedPreferencesManager
     private lateinit var vibrator: Vibrator
+    private lateinit var jsonObject: JSONObject
+
+    fun <T> addtoRequestQueue(request: Request<T>){
+        requestQueue.add(request)
+    }
+
+    private val requestQueue: RequestQueue by lazy {
+        Volley.newRequestQueue(this)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
@@ -31,6 +48,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, User_Login_Activity::class.java))
             finish()
         }
+
+        checkTokenStatus()
 
 
         //Default Fragment on StartUp
@@ -46,18 +65,55 @@ class MainActivity : AppCompatActivity() {
 
             return@setOnItemSelectedListener true
         }
-
-
-
-
-
     }
 
-   private fun replaceFragment(fragment: Fragment){
+    private fun replaceFragment(fragment: Fragment){
        val fragmentManager=supportFragmentManager
        val fragmentTransaction=fragmentManager.beginTransaction()
        fragmentTransaction.replace(R.id.frameLayout, fragment)
        fragmentTransaction.commit()
        vibrator.vibrate(50)
    }
+
+
+    private fun checkTokenStatus() {
+        jsonObject= JSONObject()
+        val url = "https://gharaanah.onrender.com/engineering/tokenauth"
+        val request = object : StringRequest(
+            Method.POST, url,
+            { response ->
+                val isTokenValid = response.toBoolean()
+
+                if (isTokenValid){
+                    //Nothing to do
+                    //Toast.makeText(this, "Login Valid", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "Login Expired", Toast.LENGTH_SHORT).show()
+                    logout()
+                }
+            },
+            {
+                Toast.makeText(this, "Some Error Occurred", Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            // Override getHeaders to add the Authorization header
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                val token = SharedPreferencesManager.getUserToken()
+                headers["Authorization"] = "Bearer $token"
+                return headers
+            }
+        }
+
+        addtoRequestQueue(request)
+
+    }
+
+    private fun logout() {
+        SharedPreferencesManager.updateLoginState(false)
+        SharedPreferencesManager.updateUserToken("")
+        startActivity(Intent(this, User_Login_Activity::class.java))
+        finish()
+    }
 }
--- app/src/main/java/com/surajverma/nitahelpers/Profile_Fragment.kt
+++ app/src/main/java/com/surajverma/nitahelpers/Profile_Fragment.kt
@@ -91,45 +91,48 @@ class Profile_Fragment : Fragment() {
             Method.GET, url, jsonObject,
             { jsonData ->
                 val action=jsonData.getBoolean("action")
-                val orderNITList=jsonData.getJSONArray("orderNITList")
+                if(action==true){
+                    val orderNITList=jsonData.getJSONArray("orderNITList")
+                    for (i in (orderNITList.length() - 1) downTo 0) {
+                        val orderObject = orderNITList.getJSONObject(i)
+                        // Extract specific fields from the orderObject
+                        val orderId = orderObject.getString("orderId")
+                        val phoneNo=orderObject.getString("phoneNo")
+                        val type = orderObject.getString("type")
+                        var store = orderObject.getString("storeName")
+                        val orderTime = orderObject.getString("orderTime")
+                        val orderstatus = "Status: "+orderObject.getString("orderStatus")
+                        val orderDetails = orderObject.getString("orderDetails")
+                        val orderPoint = orderObject.getString("orderPoint")
 
-                for (i in (orderNITList.length() - 1) downTo 0) {
-                    val orderObject = orderNITList.getJSONObject(i)
-                    // Extract specific fields from the orderObject
-                    val orderId = orderObject.getString("orderId")
-                    val phoneNo=orderObject.getString("phoneNo")
-                    val type = orderObject.getString("type")
-                    var store = orderObject.getString("storeName")
-                    val orderTime = orderObject.getString("orderTime")
-                    val orderstatus = "Status: "+orderObject.getString("orderStatus")
-                    val orderDetails = orderObject.getString("orderDetails")
-                    val orderPoint = orderObject.getString("orderPoint")
+                        // Image Allocation
+                        var image=R.drawable.amazon
+                        if(store=="amazon"){
+                            image=R.drawable.amazon
+                        }
+                        else if(store=="flipkart"){
+                            image=R.drawable.flipkart
+                        }
+                        else if(store=="Samrat"){
+                            image=R.drawable.flipkart
+                        }
+                        else if(store=="John"){
+                            image=R.drawable.john
+                        }
+                        else if(store=="Joydip"){
+                            image=R.drawable.wow
+                        }
 
-                    // Image Allocation
-                    var image=R.drawable.amazon
-                    if(store=="amazon"){
-                         image=R.drawable.amazon
-                    }
-                    else if(store=="flipkart"){
-                        image=R.drawable.flipkart
-                    }
-                    else if(store=="Samrat"){
-                        image=R.drawable.flipkart
-                    }
-                    else if(store=="John"){
-                        image=R.drawable.john
-                    }
-                    else if(store=="Joydip"){
-                        image=R.drawable.wow
-                    }
+                        store = store.substring(0,1).toUpperCase()+store.substring(1)
 
-                    store = store.substring(0,1).toUpperCase()+store.substring(1)
+                        // Create a myRequest_model object and add it to the ArrayList
+                        arrMyRequest.add(myRequest_model(image,orderId, type, store, orderTime, orderstatus, orderDetails, orderPoint))
 
-                    // Create a myRequest_model object and add it to the ArrayList
-                    arrMyRequest.add(myRequest_model(image,orderId, type, store, orderTime, orderstatus, orderDetails, orderPoint))
+                    }
+                    adapter.notifyDataSetChanged()
 
                 }
-                adapter.notifyDataSetChanged()
+
 
                 binding.recyclerViewProgressBar.visibility=View.INVISIBLE
                 Log.d("my-requests", "$jsonData")
@@ -149,7 +152,6 @@ class Profile_Fragment : Fragment() {
                 return headers
             }
         }
-
         addtoRequestQueue(request)
 
     }
