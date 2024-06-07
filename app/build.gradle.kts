00be4e270c89658aaa1e1cd3cebe41a4d5d4f2ce
Suraj <thesurajsite@gmail.com>
2024-06-08 00:09:34 +0530

    Select hostel error resolved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.surajverma.nitahelpers"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -49,8 +49,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    //Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
 
     //GSON Converter
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
